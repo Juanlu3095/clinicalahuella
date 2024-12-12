2b2f5da3092e5a5391e6ff762649587f2aa1345b
Juan Luis Talavera Peláez <jcooldevelopment@gmail.com>
2024-12-12 15:02:14 +0100

    Inicio proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./client/home/home.component').then((m) => m.HomeComponent),
+    },
+    {
+        path: 'contacto',
+        loadComponent: () => import('./client/contact/contact.component').then((m) => m.ContactComponent),
+    },
+    {
+        path: 'reservar-cita',
+        loadComponent: () => import('./client/book/book.component').then((m) => m.BookComponent),
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./client/notfound/notfound.component').then((m) => m.NotfoundComponent),
+    },
+];
---
+++ src/app/client/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../partials/header/header.component';
+import { FooterComponent } from '../../partials/footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
