e156f817dcc493b6308b70174cb43bf5a58b4b54
Juan Luis Talavera Peláez <jcooldevelopment@gmail.com>
2025-08-05 12:57:00 +0200

    banner consentimiento cookies v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { afterRender, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBottomSheet, MatBottomSheetModule, MatBottomSheetRef, } from '@angular/material/bottom-sheet';
+import { CookiebannerComponent } from './partials/cookiebanner/cookiebanner.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatBottomSheetModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'lahuella';
+  consentimiento: string | null | undefined = ''
+  private cookieBanner = inject(MatBottomSheet);
+  private readonly platformId = inject(PLATFORM_ID)
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.consentimiento = localStorage.getItem('consentimiento_ga')
+      if (!this.consentimiento) {
+        this.openCookieBanner()
+      }
+    }
+  }
+
+  openCookieBanner(): void {
+    this.cookieBanner.open(CookiebannerComponent, {
+      panelClass: 'banner-consentimiento'
+    });
+  }
 }
---
+++ src/app/partials/cookiebanner/cookiebanner.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-cookiebanner',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './cookiebanner.component.html',
+  styleUrl: './cookiebanner.component.scss'
+})
+export class CookiebannerComponent{
+  cookieService = inject(CookieService)
+  private _bottomSheetRef = inject<MatBottomSheetRef<CookiebannerComponent>>(MatBottomSheetRef); // Referencia al banner
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss(); // Se cerrará el banner al pulsar un botón
+    event.preventDefault();
+  }
+  
+  aceptarCookies() {
+    localStorage.setItem('consentimiento_ga', 'true')
+  }
+
+  denegarCookies() {
+    localStorage.setItem('consentimiento_ga', 'false')
+    this.cookieService.delete('_ga')
+    this.cookieService.delete('_ga_3ZGKXK440B')
+  }
+}
